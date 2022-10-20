6ef749c765ba4e206d3b52f4b0bbd11dbbc97094
Erik Bremen <strtok@gmail.com>
2022-10-20 07:26:52 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,42 @@ use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //
     // Start rtl_433
+    //
     let mut cmd = Command::new("rtl_433");
-
     cmd.stdout(Stdio::piped());
-    let mut child = cmd.arg("-F").arg("json").spawn()
+
+    let mut rtl_sdr_child = cmd
+        .arg("-F")
+        .arg("json")
+        .spawn()
         .expect("failed to spawn command");
-    let stdout = child.stdout.take()
+
+    let rtl_sdr_stdout = rtl_sdr_child
+        .stdout
+        .take()
         .expect("child did not have a handle to stdout");
-    let mut reader = BufReader::new(stdout).lines();
 
-    // Ensure the child process is spawned in the runtime so it can
-    // make progress on its own while we await for any output.
+    let mut reader = BufReader::new(rtl_sdr_stdout).lines();
+
+    //
+    // Asynchronously wait for the child to exit
+    //
     tokio::spawn(async move {
-        let status = child.wait().await
-            .expect("child process encountered an error");
+        let status = rtl_sdr_child
+            .wait()
+            .await
+            .expect("rtl_443 encountered an error");
 
-        println!("child status was: {}", status);
+        println!("rtl_443 status was: {}", status);
     });
 
     while let Some(line) = reader.next_line().await? {
-        println!("Line: {}", line);
+        println!("line: {}", line);
     }
-    
+
     // Start metrics webserver
     let app = Router::new().route("/metrics", get(handler));
 
